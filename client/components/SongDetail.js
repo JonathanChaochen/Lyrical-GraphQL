9811643a6b6f91295c4e5a2c674d4016297f0c18
chao chen <chaochen42@gmail.com>
2019-04-07 20:33:55 +1200

    init Lyric create form

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -3,20 +3,21 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { graphql, Query } from 'react-apollo';
 import fetchSong from '../queries/fetchSong';
+import LyricCreate from './LyricCreate';
 
 class SongDetail extends Component {
   render() {
     return (
       <div>
         <Link to="/">Back</Link>
-        <h3>Song Detail</h3>
         <Query query={fetchSong} variables={{ id: this.props.match.params.id }}>
           {({ loading, error, data }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error :(</p>;
-            return <p>{data.song.title}</p>;
+            return <h3>{data.song.title}</h3>;
           }}
         </Query>
+        <LyricCreate />
       </div>
     );
   }
